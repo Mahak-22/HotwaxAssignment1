2e1aca9e2d6a92dd2783c8b91f2187f92361c419
Mahak-22 <mahakraniadwani@gmail.com>
2026-01-16 20:58:56 +0530

    Refactor Contact_Mech class to ContactMech
    Updated class and variable names for consistency and clarity.

--- Contact_Mech.java
+++ Contact_Mech.java
@@ -3,30 +3,33 @@ package com.example.product_management_system.model;
 import jakarta.persistence.*;
 
 @Entity
-public class Contact_Mech
+public class ContactMech
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int contact_mech_id;
+    private int contactMechId;
 
     @ManyToOne
     @JoinColumn(name="customer_id", referencedColumnName = "customer_id", nullable = false)
     private Customer customer;
 
-    private String street_address;
+    @Column(length = 100, nullable = false)
+    private String streetAddress;
+
+    @Column(length = 50, nullable = false)
     private String city;
+
+    @Column(length = 50, nullable = false)
     private String state;
-    private String postal_code;
-    private String phone_number;
+
+    @Column(length = 20, nullable = false)
+    private String postalCode;
+
+    @Column(length = 20, nullable = false)
+    private String phoneNumber;
+
+    @Column(length = 100, nullable = false)
     private String email;
-
-    public int getContact_mech_id() {
-        return contact_mech_id;
-    }
-
-    public void setContact_mech_id(int contact_mech_id) {
-        this.contact_mech_id = contact_mech_id;
-    }
 
     public Customer getCustomer() {
         return customer;
@@ -34,14 +37,6 @@ public class Contact_Mech
 
     public void setCustomer(Customer customer) {
         this.customer = customer;
-    }
-
-    public String getStreet_address() {
-        return street_address;
-    }
-
-    public void setStreet_address(String street_address) {
-        this.street_address = street_address;
     }
 
     public String getCity() {
@@ -60,27 +55,43 @@ public class Contact_Mech
         this.state = state;
     }
 
-    public String getPostal_code() {
-        return postal_code;
-    }
-
-    public void setPostal_code(String postal_code) {
-        this.postal_code = postal_code;
-    }
-
-    public String getPhone_number() {
-        return phone_number;
-    }
-
-    public void setPhone_number(String phone_number) {
-        this.phone_number = phone_number;
-    }
-
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public int getContactMechId() {
+        return contactMechId;
+    }
+
+    public void setContactMechId(int contactMechId) {
+        this.contactMechId = contactMechId;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
