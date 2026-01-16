1565d7e0da806421b590bf39b1b4d9551f5c16cd
Mahak-22 <mahakraniadwani@gmail.com>
2026-01-16 21:00:34 +0530

    Update OrderController.java

--- OrderController.java
+++ OrderController.java
@@ -1,8 +1,11 @@
 package com.example.product_management_system.controller;
 
 
-import com.example.product_management_system.model.Order_Item;
+import com.example.product_management_system.model.OrderHeader;
+import com.example.product_management_system.model.OrderItem;
+import com.example.product_management_system.repo.OrderHeaderRepo;
 import com.example.product_management_system.repo.OrderItemRepo;
+import com.example.product_management_system.service.OrderHeaderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,36 +13,56 @@ import org.springframework.web.bind.annotation.*;
 public class OrderController
 {
     @Autowired
+    private OrderHeaderRepo orderHeaderRepo;
+
+    @Autowired
     private OrderItemRepo orderRepo;
 
+    private final OrderHeaderService orderService;
+
+    public OrderController(OrderHeaderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/create")
+    public OrderHeader createOrder(@RequestBody OrderHeader order) {
+        return orderService.createOrder(order);
+    }
+
     @GetMapping("/order/{id}")
-    public Object getOrder(@PathVariable int id)
-    {
-        return orderRepo.findById(id);
+    public OrderHeader getOrder(@PathVariable Integer id) {
+        return orderService.getOrderById(id);
     }
 
-    @PostMapping("/addOrder")
-    public Order_Item addOrder(@RequestBody Order_Item orderItem)
-    {
-        return orderRepo.save(orderItem);
+    @PutMapping("/update/order/{id}")
+    public OrderHeader updateOrder(
+            @PathVariable Integer id,
+            @RequestBody OrderHeader order) {
+        return orderService.updateOrderAddresses(id, order);
     }
 
-    @PutMapping("/updateOrder/{id}")
-    public Order_Item updateOrder(@PathVariable int id, @RequestBody Order_Item orderItem)
-    {
-        orderItem.setOrderItemSeqId(id);
-        return orderRepo.save(orderItem);
+    @DeleteMapping("/remove/order/{id}")
+    public void deleteOrder(@PathVariable Integer id) {
+        orderService.deleteOrder(id);
     }
 
-    @DeleteMapping("/orders/{orderId}/items/{orderItemSeqId}")
-    public void removeItem(@PathVariable int orderId, @PathVariable int itemSeqId)
-    {
-        orderRepo.findByOrderItemSeqIdAndOrderHeader_OrderId(itemSeqId, orderId);
+    @PostMapping("/add/item/{orderId}/items")
+    public OrderItem addItem(
+            @PathVariable Integer orderId,
+            @RequestBody OrderItem item) {
+        return orderService.addOrderItem(orderId, item);
     }
 
-    @DeleteMapping("/deleteOrder/{id}")
-    public void removeOrder(@PathVariable int id)
-    {
-        orderRepo.deleteById(id);
+    @PutMapping("/update/items/{itemId}")
+    public OrderItem updateItem(
+            @PathVariable Integer itemId,
+            @RequestBody OrderItem item) {
+        return orderService.updateOrderItem(itemId, item);
     }
+
+    @DeleteMapping("/remove/item/{itemId}")
+    public void deleteItem(@PathVariable Integer itemId) {
+        orderService.deleteOrderItem(itemId);
+    }
+
 }
